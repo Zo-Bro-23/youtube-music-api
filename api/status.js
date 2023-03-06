7ec42ed5ae8ffafbfc17bb75a401abe1858b1e3b
Zohan Subhash <zohan.subhash@gmail.com>
2023-03-06 14:49:28 +0530

    Fix

--- api/status.js
+++ api/status.js
@@ -35,7 +35,7 @@ export default async (req, res) => {
             const statusCollection = db.collection('status')
             const status = await statusCollection.find({ _id: key }).toArray()
 
-            if (status.expiry < Date.now() || status.length == 0) {
+            if (status.length == 0 || status[0].expiry < Date.now()) {
                 return res.send('No status!')
             }
 
